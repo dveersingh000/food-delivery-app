aa3215cf0a81746623c5a2f5b38e2ecb1e9828bd
Dharamveer <dveersingh000@gmail.com>
2024-12-02 12:17:30 +0530

    added vercel.json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './pages/Auth/Login'
 import Signup from './pages/Auth/Signup'
+import HomePage from "./pages/Home/HomePage";
 
 function App() {
 
   return (
-    <div>
-      <Login />
-      {/* <Signup /> */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   )
 }
 
